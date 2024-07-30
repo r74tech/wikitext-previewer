3f4e19012b932b480a0568e145de1b961e333417
r74tech <r74tech@users.noreply.github.com>
2024-07-30 13:10:50 +0000

    deploy: bfa23e85d9542cf26cfac575446ad323f1b9fc1c

--- ftml.web.worker.js
+++ ftml.web.worker.js
@@ -1,11 +1,27 @@
-import { init, renderHTML, ready, loading } from './lib/ftml-wasm/esm/wj-ftml-wasm.esm.js';
+// import { init, renderHTML, ready, loading, makeInfo, WikitextSettings } from './lib/ftml-wasm/esm/wj-ftml-wasm.js';
+import { init, renderHTML, ready, loading, makeInfo } from './lib/ftml-wasm/esm/wj-ftml-wasm.js';
+
 
 init();
+
 onmessage = async (e) => {
   if (!ready) await loading;
   const ftmlSource = e.data.value;
-
-  const { html, styles } = renderHTML(ftmlSource);
+  // const settings = WikitextSettings.from_mode({ mode: 'page', layout: 'wikidot' });
+  const info = makeInfo({ score: 0 });
+  // const htmlOutput = renderHTML(ftmlSource, info, settings);
+  const htmlOutput = renderHTML(ftmlSource, info);
+  const html = htmlOutput.html;
+  const styles = extractStyles(html);
   const type = e.data.type;
+
   postMessage({ html, styles, type });
 };
+
+function extractStyles(html) {
+  const styleMatches = html.match(/<style[^>]*>([^<]*)<\/style>/g);
+  return styleMatches ? styleMatches.map(style => style.replace(/<\/?style[^>]*>/g, '')) : [];
+}
+
+// v0.0.2-beta 2024-07-30
+// fix wj-ftml-wasm.js func ftom_mode, add default layout variable
